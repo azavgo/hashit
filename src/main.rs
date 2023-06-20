e19be09cebac7b122ce2e08fe1b0fbf3e93e705a
Alex. Z <azavgo@yahoo.com>
2023-06-20 20:41:41 +1000

    MD5 padding function add-on for 56 bytes messages

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,47 @@ fn hash_md5(s: &str) -> &str{
     unimplemented!();
 }
 //Will work correctly on the 64-bit OS, not sure about 32-bit OS
+//Padding format for the last block of 64 bytes: 
+//message, then 1 (binary: 10000000), then zeros, and then final 8 bytes 
+//are reserved for the length of the message itself. 
+//Example: input: "a", padding output "a10...00000001"
+//Padding format in this example: "a" - message = 1 byte, "1" = 1 byte, then 54x"0" = 54 bytes, then "00000001" - 8 bytes, 
+//all together 64 bytes
 fn padding(input: &str) -> String {
     let input_size = input.len(); //size of the input in bytes
+
     let input_size_str = input_size.to_string();//size in bytes of the input_size
     let input_size_str_size = input_size_str.len();
-    if input_size <= 56 {
-        let v: Vec<u8> = vec![0; 64 - input_size - 1 - input_size_str_size];
+
+    if input_size < 56 {
+        let v: Vec<u8> = vec![0; 64 - 1 - input_size - input_size_str_size];
+        let v_str: String = v.into_iter().map(|s| s.to_string()).collect();
+        format!("{}{}{}{}", input, "1", v_str, input_size_str)
+    } else if input_size == 56 {
+        let v: Vec<u8> = vec![0; 64 + 64 - 1 - input_size - input_size_str_size];
         let v_str: String = v.into_iter().map(|s| s.to_string()).collect();
         format!("{}{}{}{}", input, "1", v_str, input_size_str)
     } else {
         unimplemented!();
     }
-    
 }
 
 fn main() {
-    let input = "They are deterministic";
+    let input = "When the initial input is greater than 448 bits long, it";
     let output = padding(input);
-    println!("{}", output);
+    println!("input = {}, output = {}", input, output);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test00_hash_md5() {
-        assert!(hash_md5("") == "This is my output");
+    fn test01_padding() {
+        assert!(padding("abcde") == "abcde10000000000000000000000000000000000000000000000000000000005".to_string());
+    }
+    #[test]
+    fn test02_padding() {
+        assert!(padding("They are deterministic") == "They are deterministic100000000000000000000000000000000000000022".to_string());
     }
     #[test]
     fn test01_hash_md5() {
