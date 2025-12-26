6c720b9dff3d84a4b294b49702d9656bfdc5fc0a
azavgo <azavgo@yahoo.com>
2025-12-26 20:46:43 +1100

    Added SHA256 file hashing functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
-use sha256::digest;
+use sha256::{digest, try_digest};
+use std::path::Path; 
+
+#[derive(Debug)]
+pub enum HashitError {
+    Error(std::io::Error),
+}
+
+impl From<std::io::Error> for HashitError {
+    fn from(error: std::io::Error) -> Self {
+        HashitError::Error(error)
+    }
+}
 
 pub trait Hashit {
-    fn hash_sha256(&self) -> String;
+    fn hash_sha256(&self) -> Result<String, HashitError>;
+}
+
+pub struct InputFile <'a> {
+    pub input: &'a Path,
+}
+
+impl <'a> InputFile <'a> {
+    pub fn new(input: &'a Path) -> Self {
+        Self { input: input }
+    }
+}
+
+impl <'a> Hashit for InputFile <'a> {
+    fn hash_sha256(&self) -> Result<String, HashitError> {
+        Ok(try_digest(&self.input)?)
+           
+    }
 }
 
 pub struct InputString{
@@ -10,13 +39,13 @@ pub struct InputString{
 
 impl InputString {
     pub fn new(input: String) -> Self {
-        InputString { input: input }
+        Self { input: input }
     }
 }
 
 impl Hashit for InputString {
-    fn hash_sha256(&self) -> String {
-        digest(&self.input)    
+    fn hash_sha256(&self) -> Result<String, HashitError> {
+        Ok(digest(&self.input))
     }
 }
 
@@ -26,13 +55,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test01_hash_sha256() {
+    fn test01_inputstring_hash_sha256() -> Result<(), HashitError> {
         let input = "hello".to_owned();
         let input_string = InputString::new(input);
-        let hash = input_string.hash_sha256();
+        let hash = input_string.hash_sha256()?;
         assert_eq!(
             hash,
             "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
         );
+        Ok(())
+    }
+
+    #[test]
+    fn test01_inputfile_hash_sha256() -> Result<(), HashitError> {
+        let input = Path::new("./input_file_test.txt");
+        let input_file = InputFile::new(input);
+        let hash = input_file.hash_sha256()?;
+        assert_eq!(
+            hash,
+            "6d2dd541398faa91d8e29d0940e0ad602dcabc9df6b5b6f71247fb326bcd311d"
+        );
+        Ok(())
     }
 }
